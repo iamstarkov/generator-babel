98108abc607b94f5830d1848bece0da8a7a41213
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-05 03:57:46 +0100

    code: implement proper prefixes for plugins

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,8 +4,9 @@ var objectAssign = require('object-assign');
 var merge = objectAssign.bind(null, {});
 var stringify = function stringify(obj) { return JSON.stringify(obj, null, 2); };
 var parse = JSON.parse.bind(JSON);
-var concat = function concat(arr1, arr2) { return [].concat(arr1, arr2); };
+var concat = function concat(arr1, arr2, arr3) { return [].concat(arr1, arr2, arr3); };
 var prefixPresets = function prefixPresets(name) { return 'babel-preset-' + name; };
+var prefixPlugins = function prefixPlugins(name) { return 'babel-plugin-' + name; };
 var endline = function endline(str) { return str + '\n'; };
 
 module.exports = yeoman.generators.Base.extend({
@@ -27,7 +28,8 @@ module.exports = yeoman.generators.Base.extend({
       var result = merge(existing, defaults, optional);
       this.devDepsToInstall = concat(
         ['babel-cli', 'babel-core'],
-        result.presets.map(prefixPresets)
+        (result.presets || []).map(prefixPresets),
+        (result.plugins || []).map(prefixPlugins)
       );
       this.fs.write(
         this.destinationPath('.babelrc'),
